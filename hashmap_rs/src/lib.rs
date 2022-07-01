e53171c93c98fa13c445317064a276f135f40165
Yazeed1s <Yazeed1s@users.noreply.github.com>
2022-07-01 18:45:37 -0400

    implemented len(), clear(), from()

--- hashmap_rs/src/lib.rs
+++ hashmap_rs/src/lib.rs
@@ -4,27 +4,37 @@
 //     vec: Vec<(K, V)>,
 // }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 struct HashMap<K, V> {
     bucket: Vec<(K, V)>,
 }
 
-impl <K, V: Ord> HashMap <K, V> where {
+impl<K, V: Ord> HashMap<K, V> {
     fn new() -> Self {
-        HashMap {
-            bucket: Vec::new()
-        }
+        HashMap { bucket: Vec::new() }
+    }
+
+    fn from(bucket: Vec<(K, V)>) -> Self {
+        HashMap { bucket }
+    }
+
+    fn size(&self) -> usize {
+        self.bucket.len()
+    }
+
+    fn clear(&mut self) {
+        self.bucket.clear();
     }
     fn insert(&mut self, key: K, value: V) -> bool {
         self.bucket.push((key, value));
         !self.bucket.is_empty()
-        // TODO: calculate hash 
-        // TODO: handle collision 
+        // TODO: calculate hash
+        // TODO: handle collision
     }
     fn is_empty(&self) -> bool {
         self.bucket.is_empty()
     }
+    fn remove_by_key(key: K) {}
     // TODO: add resize method
     // TODO: add get method
     // TODO: add remove_by_key method
@@ -36,9 +46,15 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-       let mut map = HashMap::new();
-       map.insert(10, 14);
-       let expected = map.is_empty();
-       assert_eq!(expected, false);
+        let mut map = HashMap::new();
+        map.insert(10, 14);
+        let x: i32 = 10;
+        let expected = map.is_empty();
+        assert_eq!(expected, false);
+    }
+    #[test]
+    fn test_initilizating_using_from() {
+        let n: HashMap<&str, i32> = HashMap::from(vec![("8", 2), ("9", 4), ("8", 6), ("0", 8)]);
+        assert_eq!(n.size(), 4)
     }
 }
