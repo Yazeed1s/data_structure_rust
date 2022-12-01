7fe39555158a36e4057ddad11340c58f01aef494
Yazeed1s <106896327+Yazeed1s@users.noreply.github.com>
2022-11-30 21:12:59 -0500

    implement insert(), getKey(), remove(), len(), etc

--- hashmap_rs/src/lib.rs
+++ hashmap_rs/src/lib.rs
@@ -2,10 +2,7 @@
 #![allow(unused_variables)]
 
 use core::hash::Hash;
-use std::{collections::hash_map::DefaultHasher, hash::Hasher};
-// use core::marker::Copy;
-// use std::collections::hash_map::DefaultHasher;
-//use std::hash::Hasher;
+use std::{borrow::Borrow, collections::hash_map::DefaultHasher, hash::Hasher, ops::Index};
 
 const BUCKET_SIZE: usize = 32;
 const THRESHOLD: f32 = 0.75;
@@ -106,124 +103,67 @@ where
         /* hasher.finish() as usize % self.cap */
         index
     }
-    // TODO: add inser()
-    // TODO: add get()
+    // TODO: add insert()
+    fn insert(&mut self, key: K, value: V) -> Option<V> {
+        let index: u64 = self.hash(&key);
+        let insert = self.bucket[index as usize].insert(key, value);
+        if insert.is_none() {
+            self.items += 1;
+        }
+        insert
+    }
+    // TODO: add getKey()
+    fn get<Q>(&self, key: &Q) -> Option<&V>
+    where
+        K: Borrow<Q> + PartialEq<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let index: u64 = self.hash(key);
+        let key = self.bucket[index as usize].get(key);
+        key
+    }
     // TODO: add contains_key()
+    fn contains_key<Q>(&self, key: &Q) -> bool
+    where
+        K: Borrow<Q> + PartialEq<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        if self.get(key).is_some() {
+            return true;
+        }
+        false
+    }
     // TODO: add remove()
+    fn remove<Q>(&mut self, key: &Q) -> Option<V>
+    where
+        K: Borrow<Q> + PartialEq<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let index = self.hash(key);
+        let removed_result = self.bucket[index as usize].remove(key);
+        if removed_result.is_some() {
+            self.items -= 1;
+        }
+        removed_result
+    }
     // TODO: add len()
+    fn len(&self) -> usize {
+        self.items
+    }
 }
-// impl<K, V> HashMap<K, V>
-// where
-//     K: Hash + PartialEq + PartialOrd + Copy,
-//     V: Ord,
-// {
-//     fn new() -> Self {
-//         let mut bucket = Vec::with_capacity(BUCKET_SIZE);
-//         for _ in 0..BUCKET_SIZE {
-//             bucket.push(Vec {
-//                 buf: RawVec::NEW,
-//                 len: 0,
-//             });
-//         }
-//         Self {
-//             bucket: Vec::new(),
-//             items: 0,
-//         }
-//     }
 
-//     // fn from(bucket: Vec<(K, V)>) -> Self {
-//     //     /*  HashMap { bucket:  } */
-//     // }
+impl<K, Q, V> Index<&Q> for HashMap<K, V>
+where
+    K: Eq + Hash + Borrow<Q> + PartialEq<Q>,
+    Q: Eq + Hash + ?Sized,
+{
+    type Output = V;
 
-//     fn size(&self) -> usize {
-//         // self.bucket.len()
-//         self.items
-//     }
+    fn index(&self, index: &Q) -> &Self::Output {
+        self.get(index)
+            .expect("HashMap has no value associated to this key")
+    }
+}
 
-//     fn is_empty(&self) -> bool {
-//         //self.bucket.is_empty()
-//         self.items == 0
-//     }
-
-//     // hash the key
-//     fn hash_key(&self, key: &K) -> u64 {
-//         use std::hash::Hasher;
-//         let mut hasher = DefaultHasher::new();
-//         key.hash(&mut hasher);
-//         let hash = hasher.finish();
-//         // let index = hash % (BUCKET_SIZE as u64);
-//         let index = hash % (self.bucket.len() as u64);
-//         index
-//     }
-
-//     // map key to an index
-//     fn key_to_bucket(&mut self, key: &K) -> Option<usize> {
-//         if self.bucket.is_empty() {
-//             return None;
-//         }
-//         let index = self.hash_key(key);
-//         Some(index as usize)
-//     }
-
-//     fn clear(&mut self) {
-//         self.bucket.clear();
-//     }
-
-//     // fn insert<Ord>(&mut self, key: K, value: V) -> Option<K> {
-//     //     let bucket = self.key_to_bucket(&key);
-//     //     for i in self.bucket.iter_mut() {
-//     //         let (&mut _val, &_key): &mut Vec<(K, V)> = i;
-//     //     }
-//     //     std::mem::swap(key, value);
-//     //     // self.bucket.push((key, value));
-//     //     // !self.bucket.is_empty()
-//     //     // TODO: calculate hash
-//     //     // TODO: handle collision
-//     //     Some(key)
-
-//     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-//         // if self.buckets.is_empty() || self.items < 3 * self.buckets.len() / 4 {
-//         //     self.resize();
-//         // }
-//         let idx_bucket: usize = self.key_to_bucket(&key) as usize;
-//         let bucket = &mut self.bucket[idx_bucket];
-//         // for &mut (ref ekey, ref mut evalue) in bucket.iter_mut()
-//         for item in bucket.iter_mut() {
-//             let (ref ekey, ref mut evalue) = item;
-//             if ekey == &key {
-//                 return Some(std::mem::replace(evalue, value));
-//             }
-//         }
-//         bucket.push((key, value));
-//         self.items += 1;
-//         return None;
-//     }
-
-//     fn resize(&mut self) {
-//         let new_size = 2 * self.bucket.len();
-//     }
-// }
-
-// // fn remove_by_key(key: K) {}
-// // TODO: add resize method
-// // TODO: add get method
-// // TODO: add remove_by_key method
-// // TODO: add find value_value_by_key
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     #[test]
-//     fn it_works() {
-//         // let mut map = HashMap::new();
-//         // map.insert(10, 14);
-//         // let x: i32 = 10;
-//         // let expected = map.is_empty();
-//         // assert_eq!(expected, false);
-//     }
-//     #[test]
-//     fn test_initilizating_using_from() {
-//         // let n: HashMap<&str, i32> = HashMap::from(vec![("8", 2), ("9", 4), ("8", 6), ("0", 8)]);
-//         // assert_eq!(n.size(), 4)
-//     }
-// }
+// TODO: implement Iterator + IntoIterator for HashMap
+// TODO: add tests
